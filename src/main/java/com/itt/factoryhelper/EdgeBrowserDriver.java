99076c0a49c21efedd76400be8a64f46a5a3d9f2
Sudheer Kumar <sudheer@Administrators-MacBook-Pro.local>
2021-07-03 01:14:51 +0530

    ITNQA-14 Maxmimize window on browser launch

--- src/main/java/com/itt/factoryhelper/EdgeBrowserDriver.java
+++ src/main/java/com/itt/factoryhelper/EdgeBrowserDriver.java
@@ -56,6 +56,7 @@ public class EdgeBrowserDriver extends BrowserHelperFactory {
 		this.browserInfoObj.setBrowserVersion(cap.getVersion().toString());
 		this.ittDriverContext.setAttribute("PARENT_WINDOW_HANDLE_ID", this.driver.getWindowHandle());
 		this.driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
+		this.driver.manage().window().maximize();
 	}
 
 	public void invokeDriver() throws Exception {
